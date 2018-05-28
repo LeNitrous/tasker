d8a6f536073aaa7b35ccdb13dc2e3bcbbce6fd6e
LeNitrous <n20gaming2000@gmail.com>
2018-05-28 17:09:15 +0800

    fix typo again...

--- src/Logger.js
+++ src/Logger.js
@@ -9,7 +9,7 @@ class Logger {
 			var header = [
 				"-------------------------------------------------------------",
 				`Runtime Log for Tasker`,
-				`started on ${new Date.now()}`,
+				`started on ${new Date().now()}`,
 				`running on ${process.platform}`,
 				"-------------------------------------------------------------",
 			];
