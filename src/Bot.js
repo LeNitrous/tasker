2cb7c26e34a2adc78a08db44199c6ded55395f5a
LeNitrous <n20gaming2000@gmail.com>
2018-07-23 12:33:30 +0800

    fix typo

--- src/Bot.js
+++ src/Bot.js
@@ -63,7 +63,7 @@ class Tasker extends Discord.Client {
             .on("message", msg => {
                 if (msg.author.bot) return;
                 if (!msg.content.startsWith(this.prefix)) return;
-                if (this.frozen || this.ownerID.includes(msg.author.id)) return;
+                if (this.frozen && !this.ownerID.includes(msg.author.id)) return;
                 if (this.onTimeout.hasOwnProperty(msg.author.id)) {
                     var timeleft = Math.ceil((this.onTimeout[msg.author.id] - Date.now()) / 1000);
                     this.emit("userOnTimeout", msg, timeleft);
